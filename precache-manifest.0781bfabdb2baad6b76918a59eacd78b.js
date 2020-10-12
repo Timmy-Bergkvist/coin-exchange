7edf0f77fc82c5ba8cce83fe6a7cc8b00ef2ded7
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-10-12 11:32:09 +0200

    Updates

---
+++ precache-manifest.0781bfabdb2baad6b76918a59eacd78b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965de24c00495414f43e162464b3b7c3",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "fe43a51058fa4bf1428c",
+    "url": "/coin-exchange/static/css/main.e598a701.chunk.css"
+  },
+  {
+    "revision": "1629c11dcc7fd93ff133",
+    "url": "/coin-exchange/static/js/2.fe14e542.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.fe14e542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe43a51058fa4bf1428c",
+    "url": "/coin-exchange/static/js/main.924ab2a0.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "c90031ff7f4d9e49d2bfbf24b1eba01c",
+    "url": "/coin-exchange/static/media/logo.c90031ff.png"
+  }
+]);
